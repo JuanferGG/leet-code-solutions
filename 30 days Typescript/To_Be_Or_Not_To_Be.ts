5049b6e9cd5aeadbe0ae627616f6f32c78b6cd18
JuanferGG <juanfe15777@gmail.com>
2026-04-03 16:15:39 -0500

    Implement initial version of expect function for testing values

---
+++ 30 days Typescript/To_Be_Or_Not_To_Be.ts
@@ -0,0 +1,51 @@
+//? Write a function expect that helps developers test their code. 
+//? It should take in any value val and return an object with the following two functions.
+
+//? toBe(val) accepts another value and returns
+//? true if the two values === each other.
+//? If they are not equal, it should throw an error "Not Equal".
+
+//? notToBe(val) accepts another value and returns true 
+//? if the two values !== each other. 
+//? If they are equal, it should throw an error "Equal"
+
+
+//? Example 1:
+//? Input: func = () => expect(5).toBe(5)
+//? Output: {"value": true}
+//? Explanation: 5 === 5 so this expression returns true.
+
+//? Example 2:
+//? Input: func = () => expect(5).toBe(null)
+//? Output: {"error": "Not Equal"}
+//? Explanation: 5 !== null so this expression throw the error "Not Equal".
+
+//? Example 3:
+//? Input: func = () => expect(5).notToBe(null)
+//? Output: {"value": true}
+//? Explanation: 5 !== null so this expression returns true.
+
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+const expect = (sourcVal: any) => ({
+    toBe: (destVal: any) => {
+        if (destVal === sourcVal) return true;
+        throw new Error('Not Equal');
+    },
+    notToBe: (destVal: any) => {
+        if (destVal !== sourcVal) return true;
+        throw new Error('Equal');
+    },
+} as ToBeOrNotToBe);
+
+console.log(expect(5).toBe(null));
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+
+
